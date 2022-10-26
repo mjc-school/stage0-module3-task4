6fb68a24e86068d30b20a33840ff306e50d424f1
Lazizbek <81162929+Lazizbek-code@users.noreply.github.com>
2022-10-26 12:06:49 +0500

    Primitives

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 99;
+        int r = 114;
+
+        System.out.println((char) c);
+        System.out.println((char) h);
+        System.out.println((char) a);
+        System.out.println((char) r);
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.task4;
 
+import java.util.Scanner;
+
 public class IntRepresentation {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        double s = (9*a*a-5*b+2+a-7)*((a+b-(4*a*b))/2);
+        System.out.println(s);
     }
 }
